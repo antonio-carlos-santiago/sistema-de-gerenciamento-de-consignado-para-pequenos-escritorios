a7c5460a4cfdf645fee78c9b9cc2466c53b6fc57
MeuPython <22461016aa@gmail.com>
2023-10-10 17:31:36 -0400

    novo teste de armazenamento

--- db_escritorio/__init__.py
+++ db_escritorio/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 database = SQLAlchemy()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///clientes.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:hjBzejYiRbqD2RH9gWlm@containers-us-west-76.railway.app:8020/railway"
 database.init_app(app)
 
 from db_escritorio import Routes
